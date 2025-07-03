92a7e25c22d82f3e04115144caf433d59ddd027a
GustavoCPadilha <padilhacgustavo@gmail.com>
2025-07-02 21:30:22 -0300

    Add files via upload

---
+++ exercicios-java-basico/exercicios/3-classesEncapsulamento/Exercicio_3/Exercicio_3.java
@@ -0,0 +1,41 @@
+package com.mycompany.classesencapsulamento.Exercicio_3;
+
+import java.util.Scanner;
+
+public class Exercicio_3 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            MaquinaBanho pet = new MaquinaBanho();
+            boolean exit = false;
+            while (!exit)
+            {
+                System.out.println("""
+                                   1 - Dar banho no pet
+                                   2 - Abastecer com água
+                                   3 - Abastecer com shampoo
+                                   4 - Verificar nivel de água
+                                   5 - Verificar nivel de shampoo
+                                   6 - Verificar se tem pet no banho
+                                   7 - Colocar pet na maquina
+                                   8 - Retirar pet da máquina
+                                   9 - Limpar maquina
+                                   10 - Finalizar 
+                                   Digite o que deseja fazer com seu pet: """);
+                int opcao = scanner.nextInt();
+                switch (opcao)
+                {
+                    case 1 -> pet.banho();
+                    case 2 -> pet.abasteceAgua();
+                    case 3 -> pet.abasteceShampoo();
+                    case 4 -> pet.verificaAgua();
+                    case 5 -> pet.verificaShampoo();
+                    case 6 -> pet.verificaPetBanho();
+                    case 7 -> pet.colocaPet();
+                    case 8 -> pet.retiraPet();
+                    case 9 -> pet.limpaMaquina();
+                    case 10 -> exit = true;
+                    default -> System.out.println("Opção inválida. Tente novamente!");
+                }
+            }
+    }
+}
---
+++ exercicios-java-basico/exercicios/3-classesEncapsulamento/Exercicio_3/MaquinaBanho.java
@@ -0,0 +1,142 @@
+package com.mycompany.classesencapsulamento.Exercicio_3;
+
+
+public class MaquinaBanho {
+    
+    private int agua;
+    private int shampoo;
+    private boolean ocupada;
+    private boolean limpa;
+
+    public MaquinaBanho() {
+        this.agua = 0;
+        this.shampoo = 0;
+        this.ocupada = false;
+        this.limpa = true;
+    }
+    
+    public void banho()
+    {
+        if (this.isOcupada())
+        {
+            System.out.println("Tomou banho!");
+            this.setAgua(this.getAgua() - 10);
+            this.setShampoo(this.getShampoo() - 2);         
+        }
+        else
+        {
+            System.out.println("Primeiro coloque o pet na máquina de banho!");
+        }
+    }
+    
+    public void abasteceAgua()
+    {
+        if (this.getAgua() < 30)
+        {
+            System.out.println("Abasteceu a água! +2L");
+            this.setAgua(this.getAgua() + 2);      
+        }
+        else
+        {
+            System.out.println("A capacidade de água já está completa!");
+        }
+    }
+    
+    public void abasteceShampoo()
+    {
+        if (this.getShampoo() < 10)
+        {
+            System.out.println("Abasteceu o shampoo! +2L");
+            this.setShampoo(this.getShampoo() + 2);
+        }
+        else
+        {
+            System.out.println("A capacidade de shampoo já está completa!");
+        }
+    }
+    
+    public void verificaAgua()
+    {
+        System.out.printf("Nível de água atual: %dL\n", this.getAgua());
+    }
+    
+    public void verificaShampoo()
+    {
+        System.out.printf("Nível de shampoo atual: %dL\n", this.getShampoo());
+    }
+    
+    public void verificaPetBanho()
+    {
+        if (this.isOcupada())
+        {
+            System.out.println("A máquina de banho está ocupada!");
+        }
+        else
+        {
+            System.out.println("A máquina de banho não está ocupada!");
+        }
+    }
+    
+    public void colocaPet()
+    {
+        if (!this.isOcupada())
+        {
+            System.out.println("Pet colocado na máquina");
+            this.setOcupada(true);
+        }
+        else
+        {
+            System.out.println("A máquina já está ocupada!");
+        }
+    }
+    
+    public void retiraPet()
+    {
+        this.setOcupada(false);
+        this.setLimpa(false);
+    }
+    
+    public void limpaMaquina()
+    {
+        if (!this.isOcupada())
+        {
+            this.setLimpa(true);
+        }
+        else
+        {
+            System.out.println("Você precisa retirar o pet antes de limpar!");
+        }
+    }
+
+    public int getAgua() {
+        return agua;
+    }
+
+    public void setAgua(int agua) {
+        this.agua = agua;
+    }
+
+    public int getShampoo() {
+        return shampoo;
+    }
+
+    public void setShampoo(int shampoo) {
+        this.shampoo = shampoo;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public boolean isLimpa() {
+        return limpa;
+    }
+
+    public void setLimpa(boolean limpa) {
+        this.limpa = limpa;
+    }
+}
