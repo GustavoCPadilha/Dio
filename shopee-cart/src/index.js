26d501def760c4cdf335abcb841b36448ef06581
GustavoCPadilha <padilhacgustavo@gmail.com>
2026-06-28 12:13:14 -0300

    Add files via upload

---
+++ shopee-cart/src/index.js
@@ -0,0 +1,20 @@
+import * as cartService from "./services/cart.js";
+import createItem from "./services/item.js";
+
+const myCart = [];
+const myWishList = [];
+
+console.log("Welcome to your shopee cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+
+const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWishList, item2);
+
+await cartService.displayCart(myCart);
+
+await cartService.deleteItem(myCart, item2.name);
+
+await cartService.calculateTotal(myCart);
